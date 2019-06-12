6ce6f4bc456dd83e41be01f753956e29eacb0740
Nicholas Pankratov <48381479+pankratov35@users.noreply.github.com>
2019-06-12 10:41:42 +0300

    Add files via upload

---
+++ Cpp_3/Cpp_3.cpp
@@ -0,0 +1,24 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	cout << "Input string " << endl;
+	cin >> str;
+	cout << "String is " << str << endl;
+	int counter = 0;
+	for (unsigned i = 0; i = str.find(":", i), i != std::string::npos;)
+	{
+		str.replace(i, 1, ";");
+		i += 1;
+		counter++;
+	}
+	cout << "New string is " << str << endl;
+	cout << "Number of replaced symbols   " << counter;
+	return 0;
+}
